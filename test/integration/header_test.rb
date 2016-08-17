bfab0b825b115dbc333dfdf3be1d70cbe68b6276
Zachary Satoshi Feldcamp <zachary.feldcamp@me.com>
2016-08-17 00:18:05 -0400

    Add integration test for header links

---
+++ test/integration/header_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class HeaderTest < ActionDispatch::IntegrationTest
+    test "header links" do
+        get root_path
+        assert_template "application/home"
+        assert_select "#title[href=?]",        root_path
+        assert_select "#nav-home[href=?]",     home_path
+        assert_select "#nav-about[href=?]",    about_path
+        assert_select "#nav-contact[href=?]",  contact_path
+    end
+end
