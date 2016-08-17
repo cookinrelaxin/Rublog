677eeb2ab651c37b724587b8c204bb22d33bbb16
Zachary Satoshi Feldcamp <zachary.feldcamp@me.com>
2016-08-16 22:05:46 -0400

    Add basic routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def welcome
-      render html: "Welcome to Zack's Ruby Blog!"
+  def home
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-    root 'application#welcome'
+    root 'application#home'
+
+    get '/home',    to: 'application#home'
+    get '/about',   to: 'application#about'
+    get '/contact', to: 'application#contact'
 end
